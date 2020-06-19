ff992af4b72936efdf257b622b5e9c05abbba619
Rajeshudari <67119439+Rajeshudari@users.noreply.github.com>
2020-06-19 07:36:13 +0530

    Create Assignment 1_rajesh_display.py

---
+++ Assignment 1_rajesh_display.py
@@ -0,0 +1,79 @@
+In [1]:
+print("Enter variables")
+a=int(input())
+b=int(input())
+print(a*b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+Enter variables
+2
+3
+6
+-1
+6
+0.6666666666666666
+2
+8
+0
+In [2]:
+p=float(input())
+t=float(input())
+r=float(input())
+si=(p*t*r)/100
+print(si)
+1
+2
+3
+0.06
+In [10]:
+r=float(input())
+area=3.14*r**2
+print(area)
+1.2
+4.5216
+In [12]:
+b=int(input())
+h=int(input())
+area=1/2*b*h
+print(area)
+4
+2
+4.0
+In [14]:
+tc=float(input())
+tf=(9/5*tc)+32
+print(tf)
+1
+33.8
+In [17]:
+l=int(input())
+b=int(input())
+print(l*b)
+1
+22
+22
+In [16]:
+a=int(input())
+print(4*a)
+2
+8
+In [18]:
+r=float(input())
+area=2*3.14*r
+print(area)
+2
+12.56
+In [20]:
+a=int(input())
+b=int(input())
+temp=a
+a=b
+b=temp
+print(a,b)
+1
+2
+2 1
